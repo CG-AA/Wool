45dd5c02e738fcd54cad21469621cfea8bde358c
CG-AA <syankuan@gmail.com>
2024-07-01 20:27:40 +0800

    add

---
+++ WoolConfig.cmake
@@ -0,0 +1,14 @@
+cmake_minimum_required(VERSION 3.15)
+
+find_library(USOCKETS_LIBRARY uSockets PATHS /usr/local/lib REQUIRED)
+find_package(ZLIB REQUIRED)
+find_package(fmt REQUIRED)
+
+add_library(Wool::Wool STATIC IMPORTED)
+
+set_target_properties(Wool::Wool PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/include/Wool" # Adjust this path if necessary
+    IMPORTED_LOCATION "${CMAKE_INSTALL_PREFIX}/lib/libWool.a" # Adjust this path if necessary
+)
+
+set_property(TARGET Wool::Wool PROPERTY INTERFACE_LINK_LIBRARIES ${USOCKETS_LIBRARY} ZLIB::ZLIB fmt::fmt)
