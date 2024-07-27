e2fdcff31043892fdfa1f92975f7519d4f152977
CG-AA <syankuan@gmail.com>
2024-07-27 14:30:19 +0800

    VC

--- src/WoolVoice.cpp
+++ src/WoolVoice.cpp
@@ -1,32 +1,47 @@
 #include"../include/WoolVoice.hpp"
 
+namespace {
+    std::string getComponentString(const std::string& data, const std::string& key) {
+        size_t startPos = data.find(key+"\":\"");
+        if (startPos != std::string::npos) {
+            startPos += 4; // Move past the key and the colon
+            size_t endPos = data.find_first_of('\"', startPos);
+            if (endPos != std::string::npos) {
+                return data.substr(startPos, endPos - startPos);
+            }
+        }
+        SPDLOG_ERROR("Key not found: {}", key);
+    }
+}
+
 namespace Wool {
     Voice::Voice(const std::string& guild_id, const std::string& channel_id){
         : guild_id(guild_id), channel_id(channel_id) {
         }
     }
 
+    void Voice::parseVoiceServerUpdate(const std::string& data) {
+        
+        // Set VCSeUreceived to true
+    }
+    void Voice::parseVoiceStateUpdate(const std::string& data) {
+        // Parse the data and set the token
+        // Set VCStUreceived to true
+    }
+
     void Voice::connect() {
         Wool::OnVoiceUpdate = [this](std::string data) {
-            size_t startPos = data.find("\"t\":");
-            if (startPos != std::string::npos) {
-                startPos += 4; // Move past the key and the colon
-                size_t endPos = data.find_first_of(',', startPos);
-                if (endPos != std::string::npos) {
-                    std::string sValueStr = data.substr(startPos, endPos - startPos);
-                    // Convert to int or leave as string depending on your needs
-                    try {
-                        if (sValueStr == "VOICE_SERVER_UPDATE") {
-                            parseVoiceServerUpdate(data);
-                        } else if (sValueStr == "VOICE_STATE_UPDATE") {
-                            parseVoiceStateUpdate(data);
-                        }
-                    } catch (const std::invalid_argument& e) {
-                        SPDLOG_ERROR("Invalid argument: {}", e.what());
-                    } catch (const std::out_of_range& e) {
-                        SPDLOG_ERROR("Out of range: {}", e.what());
-                    }
+            std::string tVal = getComponentString(data, "t");
+            try {
+                if (tVal == "VOICE_SERVER_UPDATE") {
+                    parseVoiceServerUpdate(data);
+                } else if (tVal == "VOICE_STATE_UPDATE") {
+                    parseVoiceStateUpdate(data);
                 }
+            } catch (const std::invalid_argument& e) {
+                SPDLOG_ERROR("Invalid argument: {}", e.what());
+            } catch (const std::out_of_range& e) {
+                SPDLOG_ERROR("Out of range: {}", e.what());
             }
         };
     }
