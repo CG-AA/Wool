47c42c18d000344846e8adcca25db9cdbdf3d519
CG-AA <syankuan@gmail.com>
2025-06-10 18:13:00 +0800

    Prepare for stage0

---
+++ Wool.hpp
@@ -0,0 +1,10 @@
+namespace Wool {
+
+// Objects: Message, Poll, Channel, Guild, User...
+class Message {
+};
+
+class Wool {
+
+};
+}
---
+++ docs/stage0.md
@@ -0,0 +1,2 @@
+# Goal
+Send a message to a channel, ping a user, and send a picture.
